0f542d075791154587cb57eb8d1909942ded5a66
migueroncallo <migueroncallo@hotmail.com>
2016-05-17 21:08:22 -0500

    Handled multitouch bugs

--- app/src/main/java/com/example/migueroncallo/pasteleriafundown/VenderCupcake.java
+++ app/src/main/java/com/example/migueroncallo/pasteleriafundown/VenderCupcake.java
@@ -1,13 +1,16 @@
 package com.example.migueroncallo.pasteleriafundown;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class VenderCupcake extends AppCompatActivity {
+public class VenderCupcake extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vender_cupcake);
+
+
     }
 }
